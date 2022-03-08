b6e70d846e1edd339245f335e62fd98f0b7cf227
Liz Gehret <elizabeth.gehret@gmail.com>
2022-03-08 16:37:03 -0700

    placeholder for test method

---
+++ q2_fmt/_engraftment.py
@@ -0,0 +1,14 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2022, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+import qiime2
+
+from q2_fmt._format import TSVFileFormat, TSVFileDirFmt
+
+def _read_tsv_file(filepath):
+    pass
