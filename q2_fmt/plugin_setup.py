09d48421639eacc7a59c2584dc64a610a2646fc8
Liz Gehret <elizabeth.gehret@gmail.com>
2022-03-22 16:03:11 -0700

    registration for group_timepoints function and param descriptions

--- q2_fmt/plugin_setup.py
+++ q2_fmt/plugin_setup.py
@@ -8,17 +8,18 @@
 
 import importlib
 
-import qiime2.plugin
-from q2_types.sample_data import SampleData
+from qiime2.plugin import Str, Plugin, Metadata, TypeMap, Visualization, Bool, Choices
+from q2_types.sample_data import SampleData, AlphaDiversity
+from q2_types.distance_matrix import DistanceMatrix
 
 import q2_fmt
 from q2_fmt import TSVFileFormat, ModelTests
-from q2_fmt._engraftment import dataframe_adds_blank_column
+from q2_fmt._engraftment import dataframe_adds_blank_column, group_timepoints
 from q2_fmt._format import TSVFileDirFmt
 from q2_fmt._visualizer import hello_world
 from q2_fmt._type import GroupDist, Ordinal, Nominal
 
-plugin = qiime2.plugin.Plugin(name='fmt',
+plugin = Plugin(name='fmt',
                 version=q2_fmt.__version__,
                 website='https://github.com/qiime2/q2-fmt',
                 package='q2_diversity',
@@ -33,7 +34,7 @@ plugin.register_semantic_type_to_format(
 plugin.methods.register_function(
     function=dataframe_adds_blank_column,
     inputs={'dataframe': SampleData[ModelTests]},
-    parameters={'column_name': qiime2.plugin.Str},
+    parameters={'column_name': Str},
     outputs=[('output_dataframe', SampleData[ModelTests])],
     input_descriptions={
         'dataframe': 'The original dataframe to be modified.'
@@ -46,6 +47,39 @@ plugin.methods.register_function(
     },
     name='Modifies a dataframe with a specified blank column',
     description='This method adds a named blank column to an existing dataframe.'
+)
+
+T_input, T_ref, _ = TypeMap({
+    (DistanceMatrix, Str): Visualization,
+    (SampleData[AlphaDiversity], Bool % Choices(False)): Visualization,
+})
+
+plugin.methods.register_function(
+    function=group_timepoints,
+    inputs={'diversity_measure': T_input},
+    parameters={'metadata': Metadata, 'time_column': Str,
+                'reference_column': T_ref, 'subject_column': Str, 'control_column': Str},
+    outputs=[('timepoint_dists', GroupDist[Ordinal]),
+             ('reference_dists', GroupDist[Nominal])],
+    parameter_descriptions={
+        'metadata': 'The sample metadata.',
+        'time_column': 'The column within the `metadata` that the `diversity_measure` should be grouped by.'
+                       ' This column should contain simple integer values.',
+        'control_column': 'The column within the `metadata` that contains any relevant control group IDs.'
+                          ' Actual treatment samples should not contain any value within this column.',
+        'reference_column': 'The column within the `metadata` that contains the sample to use as a reference'
+                            ' for a given beta `diversity_measure`.'
+                            ' For example, this may be the relevant donor sample to compare against.',
+        'subject_column': 'The column within the `metadata` that contains the subject ID to be tracked against timepoints.',
+    },
+    output_descriptions={
+        'timepoint_dists': 'The distributions for the `diversity_measure`, grouped by the selected `time_column`.'
+                           ' May also contain subject IDs, if `subject_column` is provided in the `metadata`.',
+        'reference_dists': ' When control is provided, this will be the control distribution (for Alpha Diversity)'
+                           ' Or the pairwise control distances (for Beta Diversity).'
+                        #    ' When control and reference are provided, this will be the distribution of the'
+                        #    ' median distance from each control to all references.'
+    },
 )
 
 plugin.visualizers.register_function(
