0e095f2ef92afb936665e5b88fbb52e204146b68
Liz Gehret <elizabeth.gehret@gmail.com>
2022-03-30 13:42:43 -0700

    debug: more unit testing

--- q2_fmt/tests/test_engraftment.py
+++ q2_fmt/tests/test_engraftment.py
@@ -7,6 +7,7 @@
 # ----------------------------------------------------------------------------
 
 import unittest
+from numpy import float64
 import pandas as pd
 from skbio.stats.distance import DistanceMatrix
 
@@ -18,71 +19,79 @@ from q2_fmt._engraftment import group_timepoints
 class TestGroupTimepoints(TestPluginBase):
     package = 'q2_fmt.tests'
 
-    # def dataframe_adds_blank_column(self):
-    #     exp = pd.DataFrame(
-    #             [['L1S8', 'AGCTGACTAGTC', 'gut', '2008', ''],
-    #             ['L1S57', 'ACACACTATGGC', 'gut', '2009', '']],
-    #             columns=['sample-id', 'barcode-sequence', 'body-site', 'year', 'ziggy'],
-    #             dtype=object)
-
-    #     obs = (TSVFileFormat('data/test_metadata.tsv'))
-
-    #     self.assertEqual(exp, obs)
-
     def setUp(self):
         super().setUp()
 
         self.md_beta = Metadata.load(self.get_data_path('sample_metadata_donors.tsv'))
         self.md_alpha = Metadata.load(self.get_data_path('sample_metadata_alpha_div.tsv'))
 
-        self.dm = DistanceMatrix.read(self.get_data_path('dist_matrix_donors.tsv'))
-        self.alpha = pd.read_csv(self.get_data_path('alpha_div.tsv'),
-                                 index_col=False, header=0).transpose()[0]
+        self.dm = DistanceMatrix.read(self.get_data_path('dist_matrix_donors.tsv')).to_series()
+        self.alpha = pd.read_csv(self.get_data_path('alpha_div.tsv'), sep='\t', index_col=0)
 
-    def test_beta_timepoint_dist_with_donors_controls(self):
-        exp_dm = pd.DataFrame(
-            [['sampleA', '0.45', '7'],
-            ['sampleB', '0.41', '7'],
-            ['sampleC', '0.28', '9'],
-            ['sampleD', '0.78', '11'],
-            ['sampleE', '0.66', '11']],
+    def test_beta_dists_with_donors_controls(self):
+        exp_time_df = pd.DataFrame(
+            [['sampleA', '0.45', '7.0'],
+            ['sampleB', '0.40', '7.0'],
+            ['sampleC', '0.28', '9.0'],
+            ['sampleD', '0.78', '11.0'],
+            ['sampleE', '0.66', '11.0']],
             columns=['id', 'measure', 'group'],
-            dtype=object)
+            dtype=float64).set_index('id')
 
-        obs = group_timepoints(diversity_measure=self.dm,
+        exp_ref_df = pd.DataFrame(
+            [['donor1..donor2', '0.24', 'reference', 'donor1', 'donor2'],
+            ['donor1..donor3', '0.41', 'reference', 'donor1', 'donor3'],
+            ['donor2..donor3', '0.74', 'reference', 'donor2', 'donor3'],
+            ['sampleB..sampleC', '0.37', 'control1', 'sampleB', 'sampleC'],
+            ['sampleB..sampleD', '0.44', 'control1', 'sampleB', 'sampleD'],
+            ['sampleC..sampleD', '0.31', 'control1', 'sampleC', 'sampleD']],
+            columns=['id', 'measure', 'group', 'A', 'B'],
+            dtype=float64).set_index('id')
+
+        time_df, ref_df = group_timepoints(diversity_measure=self.dm,
                                metadata=self.md_beta,
                                time_column='days_post_transplant',
                                reference_column='relevant_donor',
                                control_column='control')
 
-        print(obs)
+        pd.testing.assert_frame_equal(time_df, exp_time_df)
+        pd.testing.assert_frame_equal(ref_df, exp_ref_df)
 
-    # def test_beta_ref_dist_with_donors_controls(self):
-    #     exp = pd.DataFrame(
-    #         [['sampleA', '0.450200535', '7'],
-    #         ['sampleB', '0.409489887', '7'],
-    #         ['sampleC', '0.28264371', '9'],
-    #         ['sampleD', '0.78092451', '11'],
-    #         ['sampleE', '0.66315908', '11']],
-    #         columns=['id', 'measure', 'group'],
-    #         dtype=object)
+    def test_alpha_dists_with_donors_controls(self):
+        exp_time_df = pd.DataFrame(
+            [['sampleA', '24', '7'],
+            ['sampleB', '37', '7'],
+            ['sampleC', '15', '9'],
+            ['sampleD', '6', '11'],
+            ['sampleE', '44', '11'],
+            ['sampleF', '17', '9'],
+            ['sampleG', '29', '7']],
+            columns=['id', 'measure', 'group'],
+            dtype=float64).set_index('id')
 
-    #     obs = group_timepoints(diversity_measure=self.dm,
-    #                            metadata=self.md_beta,
-    #                            time_column='days_post_transplant',
-    #                            reference_column='relevant_donor',
-    #                            control_column='control')
+        exp_ref_df = pd.DataFrame(
+            [['donor1', '32', 'reference'],
+            ['donor2', '51', 'reference'],
+            ['donor4', '19', 'reference'],
+            ['donor3', '3', 'reference']],
+            columns=['id', 'measure', 'group'],
+            dtype=float64).set_index('id')
 
-    # def test_alpha_timepoint_dist_with_donors_controls(self):
-    #     obs = group_timepoints(diversity_measure=self.alpha,
-    #                            metadata=self.md_alpha,
-    #                            time_column='days_post_transplant',
-    #                            reference_column='relevant_donor',
-    #                            control_column='control')
+        print(self.dm)
+        print('.......')
+        print(self.alpha)
+        print('.......')
+        print(self.md_alpha.to_dataframe())
 
-    # def test_alpha_ref_dist_with_donors_controls(self):
-    #     obs = group_timepoints(diversity_measure=self.alpha,
-    #                            metadata=self.md_alpha,
-    #                            time_column='days_post_transplant',
-    #                            reference_column='relevant_donor',
-    #                            control_column='control')
+        time_df, ref_df = group_timepoints(diversity_measure=self.alpha,
+                               metadata=self.md_alpha,
+                               time_column='days_post_transplant',
+                               reference_column='relevant_donor',
+                               control_column='control')
+
+        print(time_df)
+        print('..........')
+        print(ref_df)
+
+        pd.testing.assert_frame_equal(time_df, exp_time_df)
+        pd.testing.assert_frame_equal(ref_df, exp_ref_df)
