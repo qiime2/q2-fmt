4504af5c1c0d4aab98220ee4f72c6317a2b68bbc
Chloe Herman <60228108+cherman2@users.noreply.github.com>
2022-05-19 17:06:32 -0700

    fixing more whitespace issues

--- q2_fmt/plugin_setup.py
+++ q2_fmt/plugin_setup.py
@@ -44,8 +44,8 @@ T_subject, T_dependence = TypeMap({
 })
 
 T_engraft_subject, T_compare, _ = TypeMap({
-    (Bool % Choices(False), 
-    Str % Choices("reference", "all-pairwise")): Visualization,
+    (Bool % Choices(False),
+        Str % Choices("reference", "all-pairwise")): Visualization,
     (Str, Str % Choices("baseline", "consecutive")): Visualization
 })
 
