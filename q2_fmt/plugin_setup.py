40fb29b28a560d2ca909103b2e18adaec07eb8bd
Chloe Herman <60228108+cherman2@users.noreply.github.com>
2023-05-22 12:15:27 -0700

    BUG: Adds Level Delimiter Parameter (#59)

--- q2_fmt/plugin_setup.py
+++ q2_fmt/plugin_setup.py
@@ -274,7 +274,13 @@ plugin.methods.register_function(
 plugin.visualizers.register_function(
     function=q2_fmt.plot_heatmap,
     inputs={'data': GroupDist[Ordered, Matched] % Properties('peds')},
-    parameters={},
+    parameters={'level_delimiter': Str},
+    parameter_descriptions={'level_delimiter': 'If feature ids encode'
+                                               ' hierarchical information,'
+                                               ' split the levels when'
+                                               ' generating feature labels'
+                                               ' in the visualization using'
+                                               ' this delimiter.'},
     name='Plot Heatmap',
     description='',
     examples={
