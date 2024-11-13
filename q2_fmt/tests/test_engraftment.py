1b871e3fba26a9b74cc1ec27f9e526fd86a8090e
Chloe Herman <60228108+cherman2@users.noreply.github.com>
2024-11-13 12:17:47 -0700

    Update q2_fmt/tests/test_engraftment.py
    Co-authored-by: Liz Gehret <54517601+lizgehret@users.noreply.github.com>

--- q2_fmt/tests/test_engraftment.py
+++ q2_fmt/tests/test_engraftment.py
@@ -855,7 +855,7 @@ class TestPeds(TestBase):
                                         'transfered_donor_features',
                                         'total_donor_features', 'donor',
                                         'subject', 'group'])
-        peds_df = _compute_proportion(df=peds_df, type="Sample",
+        pedf_df = _compute_proportion(df=pedf_df, type="Sample",
                                       time=np.nan,
                                       reference_series=reference_series,
                                       table=table_df, metadata=metadata_df,
